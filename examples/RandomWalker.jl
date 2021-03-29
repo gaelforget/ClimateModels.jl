76be9a6be557332317fd0db16ae24fcb7d384396
gaelforget <gforget@mit.edu>
2021-03-29 13:38:28 -0400

    improve ShallowWaters.jl example

--- examples/RandomWalker.jl
+++ examples/RandomWalker.jl
@@ -1,10 +1,43 @@
-# # RandomWalker.jl
+# # Random Walker
 #
-# 	Here we setup, run and plot a two-dimensional random Walk
-#
+# Here we setup, run and plot a two-dimensional random walker path.
 
-using ClimateModels, GR
-MC=ModelConfig(model=ClimateModels.RandomWalker)
-setup(MC)
-out=launch(MC)
-plot(out[:,1],out[:,2])
+using ClimateModels, Plots, CSV, DataFrames
+
+# ### Formulate Model
+#
+# This simple model steps randomly, `N` times, on a `x,y` plane starting from `0,0`.
+
+function RandomWalker(x::AbstractModelConfig)
+    #model run
+    N=10000
+    m=zeros(N,2)
+    [m[i,j]=m[i-1,j]+rand((-1,1)) for j in 1:2, i in 2:N]
+
+    #output to file
+    df = DataFrame(x = m[:,1], y = m[:,2])
+    fil=joinpath(x.folder,string(x.ID),"RandomWalker.csv")
+    CSV.write(fil, df)
+
+    return m
+end
+
+# ### Run Model
+#
+# - `ModelConfig` defines the model into data structure `m`
+# - `setup` prepares the model to run in a temporary folder
+# - `launch` runs the `RandomWalker` model which writes results to file
+#
+# _Note: `RandomWalker` returns results also directly as an Array, but this is generally not an option for most, larger, models_
+
+m=ModelConfig(model=RandomWalker)
+setup(m)
+xy=launch(m);
+
+# ### Plot Results
+#
+# Afterwards, one often uses model output for further analysis. Here we just plot the random walker path from the output file.
+
+fil=joinpath(m.folder,string(m.ID),"RandomWalker.csv")
+output = CSV.File(fil) |> DataFrame
+plot(output.x,output.y) #, fmt=:png)
--- examples/ShallowWaters.jl
+++ examples/ShallowWaters.jl
@@ -1,29 +1,60 @@
 # # ShallowWaters.jl
 #
-# 	Here we setup, run and plot a two-dimensional shallow water model
+# Here we setup, run and plot a two-dimensional shallow water model using [ShallowWaters.jl](https://github.com/milankl/ShallowWaters.jl)
 #
 
-function config(x)
-    nx = 200
-    ny = 100
-    Lx = 2000e3
-    L_ratio = nx/ny
-    dx = Lx/nx
+using ClimateModels, Pkg, Plots, NetCDF
 
+# ### Formulate Model
+#
+
+function SWM(x)
     pth=pwd()
-    cd(joinpath(x.folder,string(tmp.ID)))
-    RunModel(;nx,Lx,L_ratio,Ndays=100,output=true)    # may take 10min depending on resolution
+    cd(joinpath(x.folder,string(x.ID)))
+    L_ratio = P.nx / P.ny
+    run_model(;P.nx,P.Lx,L_ratio,Ndays=P.nd,output=true); # may take 10min depending on resolution
     cd(pth)
 end
 
-## setup
+P=(nx = 100, ny = 50, Lx = 2000e3, nd=200) #adjustable parameters
 
-using ClimateModels, Pkg
-tmp=PackageSpec(url="https://github.com/milankl/ShallowWaters.jl")
-tmp=ModelConfig(model=tmp,configuration=config)
-setup(tmp)
+# ### Setup Model
+#
+# `ModelConfig` defines the model into data structure `sw`, which includes the online location for the model repository.
 
-## run
+pk=PackageSpec(url="https://github.com/milankl/ShallowWaters.jl")
+sw=ModelConfig(model=pk,configuration=SWM);
+
+# The `setup` function then clones the online repository to a temporary folder.
+
+setup(sw)
+
+# To ensure that the chosen package version is being used (just in case another version of the package was already installed) one can do this:
+
+pk=joinpath(sw.folder,string(sw.ID))
+Pkg.develop(path=pk);
+
+# ### Run Model
+#
+# Within the `launch` command is where the model run (`SWM`) takes place.
 
 using ShallowWaters
-launch(tmp)
+
+launch(sw);
+
+#Pkg.free("ShallowWaters")
+
+# ### Plot Results
+#
+# Afterwards, one often replays model output for further analysis. Here we just plot the random walker path from the output file.
+
+ncfile = NetCDF.open(joinpath(pk,"run0000","sst.nc"))
+sst = ncfile.vars["sst"][:,:,:]
+contourf(sst[:,:,end]',c = :grays, clims=(-1.,1.))
+
+# ```
+# anim = @animate for t ∈ 1:P.nd+1
+#    contourf(sst[:,:,t+1]',c = :grays, clims=(-1.,1.))
+# end
+# gif(anim, "sst.gif", fps = 40)
+# ```
