fe7686eadec912ebb0f2cf5943c3bb37dd2e0da7
gaelforget <gforget@mit.edu>
2021-04-16 12:01:30 -0400

    fix (?) Literate issue (cd related?)

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,16 @@
-using Documenter, Literate, ClimateModels
+using Documenter, Literate, ClimateModels, Pkg
 
 pth=@__DIR__
 lst=("defaults.jl","RandomWalker.jl","ShallowWaters.jl","MITgcm.jl","CMIP6.jl")
 for i in lst
     EXAMPLE = joinpath(pth, "..", "examples", i)
     OUTPUT = joinpath(pth, "src","generated")
+    Pkg.activate(joinpath(pth,"..","docs"))
     Literate.markdown(EXAMPLE, OUTPUT, documenter = true)
+    cd(pth)
+    Pkg.activate(joinpath(pth,"..","docs"))
     Literate.notebook(EXAMPLE, OUTPUT, execute = true)
+    cd(pth)
     #Literate.notebook(EXAMPLE, OUTPUT, flavor = :pluto)
 end
 
