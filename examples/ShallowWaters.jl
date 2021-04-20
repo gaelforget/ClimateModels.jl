52909aa7b9ff9da79098e9a6715e6d58b511a0e7
gaelforget <gforget@mit.edu>
2021-04-20 09:58:26 -0400

    improve ShallowWaters.jl example

--- examples/ShallowWaters.jl
+++ examples/ShallowWaters.jl
@@ -1,6 +1,6 @@
 # # Shallow Water Model
 #
-# Here we setup, run and plot a two-dimensional shallow water model using [ShallowWaters.jl](https://github.com/milankl/ShallowWaters.jl)
+# Here we setup, run and plot a two-dimensional shallow water model configuration from [ShallowWaters.jl](https://github.com/milankl/ShallowWaters.jl)
 #
 
 using ClimateModels, Pkg, Plots, NetCDF, Suppressor
@@ -8,9 +8,9 @@ using ClimateModels, Pkg, Plots, NetCDF, Suppressor
 # ## Formulate Model
 #
 
-pk=PackageSpec(url="https://github.com/milankl/ShallowWaters.jl")
+URL=PackageSpec(url="https://github.com/milankl/ShallowWaters.jl")
 
-P=Dict(:nx => 100, :ny => 50, :Lx => 2000e3, :nd=>200) #adjustable parameters
+parameters=Dict(:nx => 100, :ny => 50, :Lx => 2000e3, :nd=>500) #adjustable parameters
 
 function SWM(x)
     pth=pwd()
@@ -18,49 +18,48 @@ function SWM(x)
     (nx,ny)=(x.inputs[:nx],x.inputs[:ny])
     (Lx,nd)=(x.inputs[:Lx],x.inputs[:nd])
     L_ratio = nx / ny
-    @suppress run_model(;nx,Lx,L_ratio,Ndays=nd,output=true) #note: this may take 10min depending on resolution
+    @suppress run_model(;nx,Lx,L_ratio,Ndays=nd,output=true) #calling this may take several minutes (or more) depending on resolution
     cd(pth)
 end
 
 # ## Setup Model
 #
-# `ModelConfig` wraps up the model into a data structure, `sw`, which also includes e.g. the online location for the model repository.
+# `ModelConfig` wraps up the model into a data structure, `MC`, which also includes e.g. the online location for the model repository.
 
-sw=ModelConfig(model=pk,configuration=SWM,inputs=P);
+MC=ModelConfig(model=URL,configuration=SWM,inputs=parameters)
 
-# The `setup` function then clones the online repository to a temporary folder.
+# The `setup` function then clones the online repository to a temporary folder and sets up the `git` log subfolder.
 
-setup(sw)
+setup(MC);
 
-# To ensure that the chosen package version is being used (just in case another version of the package was already installed) one can do this:
+# The version of `ShallowWaters.jl` selected by `URL` should be used. The `Pkg.develop` command below should ensure that this is the case.
+#
+# _Note: if another version of the package was already installed, you may want to run `Pkg.free("ShallowWaters")` afterwards._
 
-pk=joinpath(sw.folder,string(sw.ID))
-@suppress Pkg.develop(path=pk)
+MCdir=joinpath(MC.folder,string(MC.ID))
+@suppress Pkg.develop(path=MCdir)
+using ShallowWaters
 
 # ## Run Model
 #
-# Within the `launch` command is where the model run (e.g. `SWM`) takes place.
+# The `SWM` model is run within the `launch` command which also updates the `git` log accordingly.
 
-using ShallowWaters
-
-launch(sw);
-
-#Pkg.free("ShallowWaters")
+launch(MC);
 
 # ## Plot Results
 #
 # Afterwards, one often replays model output for further analysis.
 # Here we plot the random walker path from the `netcdf` output file.
 
-ncfile = NetCDF.open(joinpath(pk,"run0000","sst.nc"))
+ncfile = NetCDF.open(joinpath(MCdir,"run0000","sst.nc"))
 sst = ncfile.vars["sst"][:,:,:]
-img=contourf(sst[:,:,end]',c = :grays, clims=(-1.,1.), frmt=:png)
+img=contourf(sst[:,:,parameters[:nd]]',c = :grays, clims=(-1.,1.), frmt=:png)
 
 # Or to create an animated `gif`
 #
 # ```
-# anim = @animate for t ∈ 1:P.nd+1
-#    contourf(sst[:,:,t+1]',c = :grays, clims=(-1.,1.))
+# anim = @animate for t ∈ 1:parameters[:nd]+1
+#     contourf(sst[:,:,t+1]',c = :grays, clims=(-1.,1.))
 # end
-# gif(anim, "sst.gif", fps = 40)
+# gif(anim, joinpath(MCdir,"run0000","sst.gif"), fps = 40)
 # ```
