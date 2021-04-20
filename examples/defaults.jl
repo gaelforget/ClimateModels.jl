cadc296a8b36d5b64092e86d3e069cba5f1a3fec
gaelforget <gforget@mit.edu>
2021-04-20 15:15:32 -0400

    improve defaults.jl example

--- examples/defaults.jl
+++ examples/defaults.jl
@@ -1,17 +1,22 @@
-# # Default Methods
+# # Default Behavior
 #
-# The defaults assume that the model is a `Julia` package downloaded 
-# (via `git clone`) from online repository (its `url`). The cloned 
-# package's `test/runtests.jl` is then used to _run the model_.
+# By default it is assumed that:
+#
+# - The model is a `Julia` package to be downloaded via `git clone` from the repository `URL`. 
+# - The cloned package's `test/runtests.jl` is then used to _run the model_.
+#
+# But it should immediately be noted that anything in the `ClimateModels.jl` interface can be customized differently. 
+#
+# This will become clear in the other examples that largely differ in the specifics while using the same, uniform, interface.
 
 using ClimateModels, Pkg
 
-url=PackageSpec(url="https://github.com/JuliaOcean/AirSeaFluxes.jl")
+#
 
-mo=ModelConfig(model=url)
-setup(mo)
-pause(mo)
-show(mo)
-launch(mo)
-monitor(mo)
-clean(mo)
+url=PackageSpec(url="https://github.com/JuliaOcean/AirSeaFluxes.jl")
+MC=ModelConfig(model=url)
+
+#
+
+setup(MC)
+launch(MC)
