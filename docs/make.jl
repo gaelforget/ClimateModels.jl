e33a538688e471bbf0161691e00fc85e11458a8e
gaelforget <gforget@mit.edu>
2021-07-24 00:08:35 -0400

    avoid errors that occur on github CI (but work otherwise !?)

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,8 @@
 using Documenter, Literate, ClimateModels, Pkg
 
 pth=@__DIR__
-lst=("defaults.jl","RandomWalker.jl","ShallowWaters.jl","MITgcm.jl","Speedy.jl","CMIP6.jl")
-lstExecute=("defaults.jl","RandomWalker.jl","ShallowWaters.jl","MITgcm.jl","CMIP6.jl")
+lst=("defaults.jl",)
+lstExecute=("defaults.jl",)
 for i in lst
     EXAMPLE = joinpath(pth, "..", "examples", i)
     OUTPUT = joinpath(pth, "src","generated")
