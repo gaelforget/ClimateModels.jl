d6320541fe08be4406b8e5c15488acaaa3fcdf35
gaelforget <gforget@mit.edu>
2021-10-17 15:19:16 -0400

    another try

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,6 @@
-using Documenter, ClimateModels, CairoMakie, Pkg, PyCall, Conda
+using Documenter, ClimateModels, CairoMakie, Pkg
 import PlutoSliderServer
 Pkg.precompile()
-Conda.pip_interop(true)
-Conda.pip("install", "fair")
 
 lst=("defaults.jl","RandomWalker.jl","ShallowWaters.jl","Hector.jl","FaIR.jl","MITgcm.jl","Speedy.jl","CMIP6.jl","IPCC.jl")
 
@@ -25,6 +23,8 @@ for i in lst
     fil_in=joinpath(@__DIR__,"..", "examples",i)
     fil_out=joinpath(@__DIR__,"build", "examples",i[1:end-2]*"html")
     PlutoSliderServer.export_notebook(fil_in)
+    #somehow with pycall, this fails at first, but second call works
+    i=="FaIR.jl" ? PlutoSliderServer.export_notebook(fil_in) : nothing
     mv(fil_in[1:end-2]*"html",fil_out)
     cp(fil_in,fil_out[1:end-4]*"jl")
 end
