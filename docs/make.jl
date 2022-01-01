45e2558bd87c282893431eacbb989df417de4bd3
gaelforget <gforget@mit.edu>
2021-12-31 20:09:39 -0500

    debug test

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,7 @@ MC=ModelConfig(model=f)
 setup(MC)
 #build(MC)
 #launch(MC)
+println("gf4")
 
 lst=("defaults.jl","RandomWalker.jl","ShallowWaters.jl","Oceananigans.jl","Hector.jl","FaIR.jl","MITgcm.jl","Speedy.jl","CMIP6.jl","IPCC.jl")
 
