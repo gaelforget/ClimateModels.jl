aa0ee9dc6f361f196893b3c393570d18026f8c6a
gaelforget <gforget@mit.edu>
2022-06-13 23:16:08 -0400

    skip running notebooks for now

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ Pkg.precompile()
 
 lst=("defaults.jl","RandomWalker.jl","ShallowWaters.jl","Oceananigans.jl","Hector.jl","FaIR.jl","MITgcm.jl","Speedy.jl","CMIP6.jl","IPCC.jl")
 
-do_run_notebooks=true
+do_run_notebooks=false
 
 makedocs(;
     modules=[ClimateModels],
