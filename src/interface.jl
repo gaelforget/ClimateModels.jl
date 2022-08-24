72ba98b18c91eee3c5b448e32bf933f2e92e837f
gaelforget <gforget@mit.edu>
2022-08-24 10:16:15 -0400

    improve docstring (not fully implemented status / monitor)

--- src/interface.jl
+++ src/interface.jl
@@ -16,7 +16,6 @@ model :: Union{Function,String,Pkg.Types.PackageSpec} = "anonymous"
 configuration :: Union{Function,String} = "anonymous"
 inputs :: OrderedDict{Any,Any} = OrderedDict{Any,Any}()
 outputs :: OrderedDict{Any,Any} = OrderedDict{Any,Any}()
-status :: OrderedDict{Any,Any} = OrderedDict{Any,Any}()
 channel :: Channel{Any} = Channel{Any}(10) 
 folder :: String = tempdir()
 ID :: UUID = UUIDs.uuid4()
