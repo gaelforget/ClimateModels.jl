3ad76582e660cba63e9e5e0142967b665c6509bb
gaelforget <gforget@mit.edu>
2022-09-10 12:11:38 -0400

    add run_one_notebook recipe and test

---
+++ test/run_one_notebook.jl
@@ -0,0 +1,37 @@
+
+"""
+    run_one_notebook(fil::String)
+
+```
+fil="notebooks/ClimateModels.jl/CMIP6.jl"
+MC=run_one_notebook(fil)
+```
+
+or
+
+```
+using ClimateModels
+
+nbs=notebooks.list()
+path0=joinpath(tempdir(),"notebooks")
+#notebooks.download(path0,nbs)
+
+nnbs=size(nbs,1)
+MCs = Array{AbstractModelConfig}(undef, nnbs);
+for ii in 1:nnbs
+  fil=joinpath(path0,nbs[ii,:folder],nbs[ii,:file])
+  MCs[ii]=run_one_notebook(fil)
+  show(MCs[ii])
+end
+```
+"""
+function run_one_notebook(fil::String)
+  reference_path=pwd()
+  MC=ModelConfig(model=basename(fil))
+  notebooks.setup(MC,fil)
+  cd(joinpath(pathof(MC),"run"))
+  include(joinpath(pathof(MC),"run","main.jl"))
+  cd(reference_path)
+  return MC
+end
+
